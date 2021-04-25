c3aa5d0ba5acb6e067bba6dd0e520b1e2c3686b0
Garvit512 <garvitarya512@gmail.com>
2021-04-25 22:41:02 +0530

    added Balanced Bracket problem

---
+++ practice/Stacks/problems/Balanced Bracket.py
@@ -0,0 +1,33 @@
+def balancedBrackets(string):
+    stack = []
+    balanced = False
+    bracketDic = {'(': ')', '{': '}', '[': ']'}
+
+    if len(string):
+        for bracket in string:
+            print("###############")
+            print(f"at '{bracket}'")
+            if bracket == '(' or bracket == '[' or bracket == '{':
+                stack.append(bracket)
+            elif bracket == ')' or bracket == ']' or bracket == '}':
+                if len(stack) == 0:
+                    balanced = False
+                elif len(stack) != 0 and bracket == bracketDic[stack[len(stack)-1]]:
+                    x = stack.pop()
+                    print(f"poping {x} and {bracket}")
+                    balanced = True
+                else:
+                    # balanced = False
+                    return False
+            print(stack)
+        if len(stack) != 0:
+            balanced = False
+    return balanced
+
+
+balancedBrackets("([])(){}(())()()")
+balancedBrackets("""([{}])(""")
+balancedBrackets("""()))""")
+balancedBrackets("""())""")
+balancedBrackets("""()""")
+balancedBrackets("""(((((({{{{{[[[[[([))""")
